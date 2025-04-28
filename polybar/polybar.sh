336d5a44699072edd85547cc22234b62255aa389
palczynski.maciej <palczynski.maciej@gmail.com>
2025-04-28 11:01:02 +0200

    Add i3 and polybar improvements

--- polybar/polybar.sh
+++ polybar/polybar.sh
@@ -6,8 +6,18 @@
 # Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log 
-polybar bar --config=~/.config/polybar/config.ini 1>&1 | tee -a /tmp/polybar1.log & disown
-
+# Launch bar and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		if [ $m = "eDP-1" ]; then
+			continue	
+		fi	
+		MONITOR=$m polybar bar2 --config=~/.config/polybar/config.ini 2>&2 | tee -a /tmp/polybar2.log & disown
+	done
+else
+	polybar --reload bar &
+fi
+polybar bar --config=~/.config/polybar/config.ini 1>&2 | tee -a /tmp/polybar1.log & disown
+	
 echo "Bars launched..."
